b4554c6bbf93a7d56d759b7e2a1dd8693c802910
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-03-28 18:10:44 -0400

    cleaner added

--- database/migrations/20190328173338_dishes_table.js
+++ database/migrations/20190328173338_dishes_table.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
-
+return knex.schema.createTable('dishes', table => {
+    //id
+    table.increments();
+    // name
+    table.string('name', 128).notNullable();
+})
 };
 
 exports.down = function(knex, Promise) {
-    
+return knex.schema.dropTableIfExists('dishes')
 };
